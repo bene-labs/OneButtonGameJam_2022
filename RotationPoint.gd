4c15af873129441f5ada008fdb9d2610667ffb67
bene-labs <62158116+bene-labs@users.noreply.github.com>
2022-11-24 10:05:39 +0100

    add rotation move speed multiplier

--- RotationPoint.gd
+++ RotationPoint.gd
@@ -3,12 +3,14 @@ extends Node2D
 export var rotation_per_seconds = 1
 export var is_rotating = false
 export var x_direction = 1 # 1 or -1
+export var move_speed_multiplier = 1.5
 
 func _process(delta):
 	if is_rotating:
 		rotate((TAU * rotation_per_seconds) * delta * x_direction)
 	
 func calc_and_set_rotation_per_seconds(speed, radius):
+	speed = speed * move_speed_multiplier
 	var circumference = 2 * radius* PI
 	rotation_per_seconds = speed / circumference
 
