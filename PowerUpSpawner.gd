aa3e85316f6ca78a525d2535c141a540e27f9549
bene-labs <62158116+bene-labs@users.noreply.github.com>
2022-11-26 20:44:46 +0100

    rework/fix PowerUpSpawner

--- PowerUpSpawner.gd
+++ PowerUpSpawner.gd
@@ -3,6 +3,8 @@ extends Node2D
 enum RespawnTypes{AFTER_COLLECTED, AFTER_ZONE_CREATED}
 
 export (PackedScene) var PowerUp
+export (NodePath) var spawn_area_path
+var SpawnArea
 #export var concurrent_powerups = 1
 export (RespawnTypes) var respawn_type = RespawnTypes.AFTER_COLLECTED
 export var spawn_chance_AFTER_ZONE_CREATED = 25
@@ -13,12 +15,13 @@ var spawns_queued = 0
 
 func get_spawn_position():
 	var spawn_position = Vector2.ZERO
-	spawn_position.x = rand_range($SpawnArea.position.x, $SpawnArea.position.x + $SpawnArea/CollisionShape2D.shape.extents.x)
-	spawn_position.y = rand_range($SpawnArea.position.y, $SpawnArea.position.y + $SpawnArea/CollisionShape2D.shape.extents.y)
+	spawn_position.x = rand_range(SpawnArea.get_node("CollisionShape2D").global_position.x, SpawnArea.get_node("CollisionShape2D").global_position.x + SpawnArea.get_node("CollisionShape2D").shape.extents.x)
+	spawn_position.y = rand_range(SpawnArea.get_node("CollisionShape2D").global_position.y, SpawnArea.get_node("CollisionShape2D").global_position.y + SpawnArea.get_node("CollisionShape2D").shape.extents.y)
 	return spawn_position
 
 func _ready():
 	randomize()
+	SpawnArea = get_node(spawn_area_path)
 	if respawn_type == RespawnTypes.AFTER_COLLECTED:
 		$PowerUpRespawnDelay.wait_time = rand_range(min_respawn_delay, max_respawn_delay)
 		$PowerUpRespawnDelay.start()
@@ -35,8 +38,8 @@ func spawn_powerup():
 	var new_power_up = PowerUp.instance()
 	new_power_up.spawned_by = self
 	new_power_up.randomize_type()
-	new_power_up.position = get_spawn_position()
-	call_deferred("add_child", new_power_up)
+	new_power_up.global_position = get_spawn_position()
+	get_tree().root.get_child(0).call_deferred("add_child", new_power_up)
 
 func queue_spawn():
 	if respawn_type != RespawnTypes.AFTER_COLLECTED:
