965b808a18e974c3d8e60bf31d0e8f571775122b
bene-labs <62158116+bene-labs@users.noreply.github.com>
2022-11-28 20:54:20 +0100

    add toggleable FPS counter

--- Game.gd
+++ Game.gd
@@ -4,8 +4,8 @@ export var restart_delay = 2.0
 export (bool) var switch_level_on_restart = false
 export var levels = []
 
-
 func _ready():
+	$FpsCounter.hide()
 	var new_timer = Timer.new()
 	new_timer.autostart = false
 	new_timer.wait_time = restart_delay
@@ -13,6 +13,10 @@ func _ready():
 	new_timer.name = "RestartTimer"
 	add_child(new_timer)
 	randomize()
+
+func _input(event):
+	if Input.is_action_just_pressed("toogle_fps"):
+		$FpsCounter.hide() if $FpsCounter.is_visible() else $FpsCounter.show()
 
 func restart_after_delay():
 	$RestartTimer.start()
