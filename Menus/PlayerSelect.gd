0f97a03c73419ce77046f76327c9bc07ae8a6717
bene-labs <62158116+bene-labs@users.noreply.github.com>
2022-12-04 23:38:36 +0100

    add Main Screen, add Blinking Text

--- Menus/PlayerSelect.gd
+++ Menus/PlayerSelect.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 export var player_sprites_paths = []
 
+func _process(delta):
+	if Input.is_action_just_pressed("back"):
+		get_tree().change_scene("res://Menus/MainScreen.tscn")
+
 func preview_selection(selection):
 	for sprite in player_sprites_paths:
 		get_node(sprite).hide()
