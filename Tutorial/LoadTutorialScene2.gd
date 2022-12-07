dd551fb9a1ddd62afec6c7b3db7886c7d583a231
BitteSushi <118819813+BitteSushi@users.noreply.github.com>
2022-12-07 07:42:17 +0100

    mod Tutorial

--- Tutorial/LoadTutorialScene2.gd
+++ Tutorial/LoadTutorialScene2.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+export (NodePath) var continueButtonPath
+export (NodePath) var playerPath
+
+func _ready():
+	get_node(continueButtonPath).hide()
 
 func _on_Anchor_wasHitLoadTutorialScene2():
+	get_node(continueButtonPath).show()
+	get_node(playerPath).set_process(false)
+	get_node(playerPath).set_physics_process(false)
+
+func loadNextScene():
 	get_tree().change_scene("res://Tutorial/TutorialScene2.tscn")
