ee52cd355da92c815a554b44d9960c607f5c2baa
bene-labs <62158116+bene-labs@users.noreply.github.com>
2022-12-16 15:09:07 +0100

    add WreathBorder to Levels, add Christmas Player sprite, mod Level select to include moon and random

--- Menus/LevelSelect.gd
+++ Menus/LevelSelect.gd
@@ -3,12 +3,20 @@ class_name LevelSelect extends Node2D
 export var preview_textures = []
 export var player_count = 2
 
+export var level_names = []
+
+func _ready():
+	randomize()
+
 func _process(delta):
 	if Input.is_action_just_pressed("back"):
 		get_tree().change_scene("res://Menus/PlayerSelect.tscn")
 
 func load_level(name):
-	get_tree().change_scene("res://Levels/%s.tscn" % name)
+	if name == "random":
+		get_tree().change_scene("res://Levels/%s.tscn" % level_names[randi() % len(level_names)])
+	else:
+		get_tree().change_scene("res://Levels/%s.tscn" % name)
 
 func set_preview_texture(index):
 	$Preview.texture = preview_textures[index]
